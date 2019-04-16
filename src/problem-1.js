09161f609d2b379e25c6bb3ceee60d1f29df4c7a
hadashi <hadashi.heitai@gmail.com>
2019-04-16 22:10:28 +0200

    Added Problem1 constructor

--- src/problem-1.js
+++ src/problem-1.js
@@ -1,3 +1,9 @@
+//Find the sum of all the multiples of 3 or 5 below 1000.
+
 export class Problem1 {
-    constructor(){};
-}
+    constructor(multiplier1, multiplier2, below) {
+        this.multiplier1 = multiplier1;
+        this.multiplier2 = multiplier2;
+        this.below = below;
+    };
+} 
