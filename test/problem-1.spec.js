3265565c9a8b3b39b2acf098976367cfe2817096
hadashi <hadashi.heitai@gmail.com>
2019-04-16 22:10:02 +0200

    Added more Problem1 tests for multiplicators

--- test/problem-1.spec.js
+++ test/problem-1.spec.js
@@ -10,6 +10,19 @@ describe("Problem 1 testing...", () => {
   });
  
   it("should pass this test", () =>{
-    expect(true).to.eql(true)
-  }) 
-})
+    expect(true).to.eql(true);
+  });
+
+  it("should return false, when passed a value not divided by 3 and 5", () =>{
+    expect(checker.isMultipleOf(11)).to.eql(false);
+  });
+  it("should return true, when passed a value not divided by 3 but divided by 5", () =>{
+    expect(checker.isMultipleOf(10)).to.eql(true);
+  });
+  it("should return true, when passed a value not divided by 5 but divided by 3", () =>{
+    expect(checker.isMultipleOf(99)).to.eql(true);
+  });
+  it("should return true, when passed a value divided by 3 and 5", () =>{
+    expect(checker.isMultipleOf(15)).to.eql(true);
+  });
+});
